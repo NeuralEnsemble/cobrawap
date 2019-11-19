5cf561c4616e6417514236c302d62f98ceecde39
Robin <robin.gutzen@live.de>
2019-11-19 16:45:21 +0100

    remove custom path setting, and provide only template config files

--- pipeline/settings.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# determine working directory (data_challenge/ directory)
-working_dir = os.path.dirname(os.path.realpath(__file__))
-
-# path for generated data
-output_path = os.path.join(os.path.expanduser('~'), 'ProjectsData/wave_analysis_pipeline/')
---
+++ pipeline/settings_template.py
@@ -0,0 +1,4 @@
+import os
+
+# path for generated data
+output_path = os.path.join(os.path.expanduser('~'), 'path/to/pipeline/output/folder/')

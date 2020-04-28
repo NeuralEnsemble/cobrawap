f8612e0cfd7da37352b83ad0f8f76ca94579f960
Robin <robin.gutzen@live.de>
2020-04-28 17:25:22 +0200

    introducing config profiles

--- pipeline/README.md
+++ pipeline/README.md
@@ -5,17 +5,17 @@ __Stage inputs__:
 The path to the input file for each stage is defined in the config parameter `STAGE_INPUT`. When executing the whole pipeline (calling `snakemake` form the `pipeline/` folder) the stage inputs are automatically set to the outputs of the previous stage, respectively.
 
 __Stage outputs__:
-The stage output file is stored as `output_path/STAGE_NAME/STAGE_OUTPUT`, with `STAGE_NAME`, and `STAGE_OUTPUT` taken from the corresponding config file and `output_path` from *settings.py*.
+The stage output file is stored as `output_path/PROFILE/STAGE_NAME/STAGE_OUTPUT`, with `PROFILE`, `STAGE_NAME`, and `STAGE_OUTPUT` taken from the corresponding config file and `output_path` from *settings.py*.
 
-When the whole pipeline is executed a summary report is created in `output_path/STAGE_NAME/report.html`. For individual stage execution it can be created with `snakemake --report /path/to/report.html`.
+When the whole pipeline is executed a summary report is created in `output_path/PROFILE/STAGE_NAME/report.html`. For individual stage execution it can be created with `snakemake --report /path/to/report.html`.
 
 __Block input__:
 Inputs to specific blocks are handled by the corresponding rule in the *Snakefile* dependent on the mechanics of the respective stage.
 
 __Block outputs__:
-All output from blocks (data and figures) is stored hierarchically in `output_path/STAGE_NAME/<block name>/`.
+All output from blocks (data and figures) is stored hierarchically in `output_path/PROFILE/STAGE_NAME/<block name>/`.
 
 ### Global Parameters
 [see config](config_template.yaml)
 
-PLOT_TSTART, PLOT_TSTOP, PLOT_CHANNEL, PLOT_FORMAT, USE_LINK_AS_STAGE_OUTPUT, NEO_FORMAT
+PLOT_TSTART, PLOT_TSTOP, PLOT_CHANNEL, PLOT_FORMAT, USE_LINK_AS_STAGE_OUTPUT, NEO_FORMAT, PROFILE
--- pipeline/scripts/snakefile_utils.py
+++ pipeline/scripts/snakefile_utils.py
@@ -1,8 +1,8 @@
 import os
 import yaml
 
-def read_stage_output(stage, working_dir, output_namespace="STAGE_OUTPUT"):
-    with open(os.path.join(working_dir, stage, 'config.yaml'), 'r') as f:
+def read_stage_output(stage, working_dir, config_name, output_namespace="STAGE_OUTPUT"):
+    with open(os.path.join(working_dir, stage, config_name), 'r') as f:
         config_dict = yaml.safe_load(f)
     if output_namespace in config_dict.keys():
         return config_dict[output_namespace]
@@ -10,10 +10,10 @@ def read_stage_output(stage, working_dir, output_namespace="STAGE_OUTPUT"):
         raise ValueError(f"config file of stage {stage} "
                        + f"does not define {output_namespace}!")
 
-def create_temp_configs(stages, working_dir, file_name='temp_config.yaml'):
+def create_temp_configs(stages, working_dir, config_name, temp_name='temp_config.yaml'):
     for i, stage in enumerate(stages):
-        config_path = os.path.join(working_dir, stage, 'config.yaml')
-        new_config_path = os.path.join(working_dir, stage, file_name)
+        config_path = os.path.join(working_dir, stage, config_name)
+        new_config_path = os.path.join(working_dir, stage, temp_name)
         with open(config_path, 'r') as f:
             config_dict = yaml.safe_load(f)
         with open(new_config_path, 'w') as f:
@@ -23,9 +23,10 @@ def create_temp_configs(stages, working_dir, file_name='temp_config.yaml'):
 def update_configfile(config_path, update_dict):
     # Careful! This function overwrites the config file.
     # Comments in the file are lost.
-    with open(config_path, 'r+') as f:
+    with open(config_path, 'r') as f:
         config_dict = yaml.safe_load(f)
-        config_dict.update(**update_dict)
+    config_dict.update(**update_dict)
+    with open(config_path, 'w') as f:
         f.write(yaml.dump(config_dict, default_flow_style=False))
     return None
 
@@ -33,7 +34,9 @@ def set_stage_inputs(stages, working_dir, output_path, config_file='temp_config.
                      intput_namespace="STAGE_INPUT"):
     update_dict = {}
     for i, stage in enumerate(stages[:-1]):
-        output_name = read_stage_output(stage, working_dir=working_dir)
+        output_name = read_stage_output(stage,
+                                        working_dir=working_dir,
+                                        config_name=config_file)
         update_dict[intput_namespace] = os.path.join(output_path, stage, output_name)
         update_configfile(config_path=os.path.join(working_dir, stages[i+1], config_file),
                           update_dict=update_dict)

24f677cd9e0e291f631547e0b3b3f8cfb5063e03
Robin <robin.gutzen@live.de>
2021-06-08 16:40:14 +0200

    fix detrending block

--- pipeline/stage02_processing/scripts/detrending.py
+++ pipeline/stage02_processing/scripts/detrending.py
@@ -5,46 +5,49 @@ import numpy as np
 import neo
 import argparse
 import os
+from copy import copy
+import warnings
 from utils import load_neo, write_neo
 
-def detrending(signal, order):
-    if isinstance(signal, neo.AnalogSignal):
-        X = signal.as_array()
-    elif isinstance(signal, pq.quantity.Quantity):
-        X = copy(signal.magnitude)
-    elif isinstance(signal, np.ndarray):
-        X = copy(signal)
-    else:
-        raise TypeError('Input signal must be either an AnalogSignal,'
-                      + 'a quantity array, or a numpy array.')
-    if not type(order) == int:
-        raise TypeError("Detrending order needs to be int.")
-    if order < 0 or order > 4:
-        raise InputError("Detrending order must be between 0 and 4!")
-
-    dim_t, num_channels = signal.shape
-    window_size = len(signal)
-
-    if order > 0:
-        X = X - np.mean(X, axis=0)
-    if order > 1:
-        factor = [1, 1/2., 1/6.]
-        for i in np.arange(2, order):
-            detrend = np.zeros_like(X)
-            for channel, x in enumerate(X.T):
-                detrend[:,channel] =\
-                    np.linspace(-window_size/2., window_size/2., window_size)**i \
-                    * np.mean(np.diff(x, n=i)) * factor[i-2]
-            X = X - detrend
-
-    if isinstance(signal, neo.AnalogSignal):
-        signal_out = signal.duplicate_with_new_data(X)
-        signal_out.array_annotate(**signal.array_annotations)
-        return signal_out
-    elif isinstance(signal, pq.quantity.Quantity):
-        return X * signal.units
-    elif isinstance(signal, np.ndarray):
-        return X
+## REPLACED BY SCIPY FUNCTION
+# def detrending(signal, order):
+#     if isinstance(signal, neo.AnalogSignal):
+#         X = signal.as_array()
+#     elif isinstance(signal, pq.quantity.Quantity):
+#         X = copy(signal.magnitude)
+#     elif isinstance(signal, np.ndarray):
+#         X = copy(signal)
+#     else:
+#         raise TypeError('Input signal must be either an AnalogSignal,'
+#                       + 'a quantity array, or a numpy array.')
+#     if not type(order) == int:
+#         raise TypeError("Detrending order needs to be int.")
+#     if order < 0 or order > 4:
+#         raise InputError("Detrending order must be between 0 and 4!")
+#
+#     dim_t, num_channels = signal.shape
+#     window_size = len(signal)
+#
+#     if order > 0:
+#         X = X - np.mean(X, axis=0)
+#     if order > 1:
+#         factor = [1, 1/2., 1/6.]
+#         for i in np.arange(2, order):
+#             detrend = np.zeros_like(X)
+#             for channel, x in enumerate(X.T):
+#                 detrend[:,channel] =\
+#                     np.linspace(-window_size/2., window_size/2., window_size)**i \
+#                     * np.mean(np.diff(x, n=i)) * factor[i-2]
+#             X = X - detrend
+#
+#     if isinstance(signal, neo.AnalogSignal):
+#         signal_out = signal.duplicate_with_new_data(X)
+#         signal_out.array_annotate(**signal.array_annotations)
+#         return signal_out
+#     elif isinstance(signal, pq.quantity.Quantity):
+#         return X * signal.units
+#     elif isinstance(signal, np.ndarray):
+#         return X
 
 
 if __name__ == '__main__':
@@ -54,13 +57,20 @@ if __name__ == '__main__':
                      help="path to input data in neo format")
     CLI.add_argument("--output",  nargs='?', type=str, required=True,
                      help="path of output file")
-    CLI.add_argument("--order", nargs='?', type=int, default=2,
+    CLI.add_argument("--order", nargs='?', type=int, default=1,
                      help="detrending order")
     args = CLI.parse_args()
 
     block = load_neo(args.data)
+    asig = block.segments[0].analogsignals[0]
 
-    asig = detrending(block.segments[0].analogsignals[0], args.order)
+    if (args.order == 0) or (args.order == 1):
+        dtrend = 'linear' if args.order else 'constant'
+        signal = scipy.signal.detrend(asig.as_array(), type=dtrend, axis=0)
+        asig = asig.duplicate_with_new_data(signal)
+        asig.array_annotate(**asig.array_annotations)
+    else:
+        warnings.warn("Detrending order must be either 0 (constant) or 1 (linear)! Skip.")
 
     asig.name += ""
     asig.description += "Detrended by order {} ({}). "\

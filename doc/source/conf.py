73c3768a601160cfe782973ac862f96b1e7bcf4d
Robin <robin.gutzen@live.de>
2023-03-10 14:31:50 +0100

    add favicon

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -89,6 +89,8 @@ html_theme_options = {
     'fixed_sidebar': 'true'
 }
 
+html_favicon = '../images/cobrawap_icon.ico'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".

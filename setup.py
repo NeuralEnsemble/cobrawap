2abd3870e4f8e38b33e911233a05d7641701135d
Cosimo Lupo <cosimo.lupo89@gmail.com>
2025-06-10 13:54:21 +0200

    Roll-back of typo fixes, so to have only formatting changes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup()     # everything configured via pyproject.toml
 
 
 #     author="Cobrawap authors and contributors",
-#     author_email="contact@cobrawap.org",
+#     author_email="robin.gutzen@outlook.com",
 #     description="Collaborative Brain Wave Analysis Pipeline (Cobrawap)",
 #
 #     entry_points={

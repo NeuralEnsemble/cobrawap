65fab97f2bf75a563dd8ff8f9c9fbfe294980795
Cosimo Lupo <cosimo.lupo89@gmail.com>
2025-09-05 15:09:10 +0200

    Addition of the biepxonential kernel and general optimization

--- cobrawap/pipeline/stage02_processing/scripts/convolution.py
+++ cobrawap/pipeline/stage02_processing/scripts/convolution.py
@@ -16,31 +16,73 @@ CLI.add_argument("--data", nargs='?', type=Path, required=True,
 CLI.add_argument("--output", nargs='?', type=Path, required=True,
                  help="path of output file")
 CLI.add_argument("--kernel_type", nargs='?', type=str, required=True,
-                 choices=["gaussian"], help="type of convolution kernel")
+                 choices=["gaussian", "biexponential"],
+                 help="type of convolution kernel")
 CLI.add_argument("--kernel_params", nargs='+', type=str, required=True,
                  help="parameters of convolution kernel")
 
 
-def gaussian_kernel(duration_ms, sampling_rate_Hz, std_dev_ms):
+def gaussian_kernel(sampling_rate_Hz, std_dev_ms, duration_ms=None):
     """
-    Generates a Gaussian kernel for smoothing.
+    Generates a Gaussian convolution kernel.
 
     Parameters:
-    - duration_ms: The duration over which the kernel is non-zero.
-    - sampling_rate_Hz: The number of samples per second.
-    - std_dev_ms: The standard deviation of the Gaussian kernel in ms.
-
+    - sampling_rate_Hz:  The number of samples per second.
+    - std_dev_ms:        The standard deviation of the Gaussian kernel in ms.
+    - duration_ms:       [Optional] The duration over which the kernel is non-zero;
+                         default value is 8 times the provided std_dev_ms,
+                         suitably rounded to the closest multiple integer
+                         of the sampling time step
     Returns:
     - Gaussian kernel array.
     """
-    sampling_duration_s = 1 / sampling_rate_Hz
-    sampling_duration_ms = sampling_duration_s * 1000.0  # ms / s
-    kernel_start_ms = -0.5*duration_ms
-    kernel_stop_ms = 0.5*duration_ms
-    t_ms = np.arange(kernel_start_ms, kernel_stop_ms+0.5*sampling_duration_ms, sampling_duration_ms)
-    gaussian = np.exp(-0.5 * (t_ms / std_dev_ms) ** 2)
-    gaussian /= gaussian.sum()  # Normalize the kernel to ensure the signal energy is preserved
-    return gaussian
+
+    sampling_dt_s = 1 / sampling_rate_Hz
+    sampling_dt_ms = sampling_dt_s * 1000
+    if duration_ms:
+        half_width = 0.5*duration_ms
+    else:
+        # default span is 4 times std_dev_ms, on both sides
+        half_width = 4*std_dev_ms
+    n = math.ceil(half_width/sampling_dt_ms)
+    times = np.linspace(-n*sampling_dt_ms, n*sampling_dt_ms, 2*n+1)
+
+    kernel = np.exp(-0.5 * (times / std_dev_ms) ** 2)
+    kernel /= np.sum(kernel)
+
+    return kernel
+
+
+def biexponential_kernel(sampling_rate_Hz, tau_rise_ms, tau_decay_ms, duration_ms=None):
+    """
+    Generates a bi-exponential convolution kernel.
+
+    Parameters:
+    - sampling_rate_Hz:  The number of samples per second.
+    - tau_rise_ms:       Time constant for the rising phase in ms.
+    - tau_decay_ms:      Time constant for the decaying phase in ms.
+    - duration_ms:       [Optional] The duration over which the kernel is non-zero;
+                         default value is 8 times the sum of the provided values
+                         for tau_rise_ms and tau_decay_ms
+
+    Returns:
+    - Biexponential kernel array.
+    """
+
+    sampling_dt_s = 1 / sampling_rate_Hz
+    sampling_dt_ms = sampling_dt_s * 1000.0
+    if duration_ms:
+        half_width = 0.5*duration_ms
+    else:
+        # default span is 4 times (tau_rise_ms + tau_decay_ms), on both sides
+        half_width = 4*(tau_rise_ms+tau_decay_ms)
+    n = math.ceil(half_width/sampling_dt_ms)
+    times = np.linspace(-n*sampling_dt_ms, n*sampling_dt_ms, 2*n+1)
+
+    kernel = (np.exp(-times / tau_decay_ms) - np.exp(-times / tau_rise_ms)) * np.heaviside(times, 0)
+    kernel /= np.sum(kernel)
+
+    return kernel
 
 
 def smooth_signal(asig, kernel_type, kernel_params):
@@ -48,18 +90,27 @@ def smooth_signal(asig, kernel_type, kernel_params):
     Smooths the given signal by convolving it with the specified kernel.
 
     Parameters:
-    - signal: The original signal
+    - signal: The original signal.
     - kernel: The smoothing kernel.
 
     Returns:
     - Smoothed signal.
     """
 
-    if kernel_type=='gaussian':
+    try:
         duration_ms = kernel_params['duration_ms']
+    except KeyError:
+        duration_ms = None
+
+    if kernel_type=='gaussian':
         sampling_rate_Hz = kernel_params['sampling_rate_Hz']
         std_dev_ms = kernel_params['std_dev_ms']
-        kernel = gaussian_kernel(duration_ms, sampling_rate_Hz, std_dev_ms)
+        kernel = gaussian_kernel(sampling_rate_Hz, std_dev_ms, duration_ms)
+    if kernel_type=='biexponential':
+        sampling_rate_Hz = kernel_params['sampling_rate_Hz']
+        tau_rise_ms = kernel_params['tau_rise_ms']
+        tau_decay_ms = kernel_params['tau_decay_ms']
+        kernel = biexponential_kernel(sampling_rate_Hz, tau_rise_ms, tau_decay_ms, duration_ms)
     else:
         kernel = None
 
